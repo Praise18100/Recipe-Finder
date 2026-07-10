ed92a22e47b62218df5526399baee83d8b46f405
Owabumuwa-Blessing <ojfeb.owa@gmail.com>
2026-07-10 22:55:13 +0100

    Add new components and styles for recipe layout, footer, and user reviews
    - Introduced new images for reviews and hero sections.
    - Created footer layout with responsive design.
    - Added Contact component with form functionality.
    - Developed Hero component with background image and recipe details.
    - Implemented RecipeSection component with search functionality and recipe cards.
    - Added Reviews component to display user testimonials with styling.
    - Created CSS files for each component to manage styles effectively.
    - Added login page styles for better user experience.
    - Established base styles for recipe page layout.

--- src/pages/recipePage.jsx
+++ src/pages/recipePage.jsx
@@ -1,7 +1,15 @@
+import Hero from "../components/recipe-layout/Hero";
+import RecipeSection from "../components/recipe-layout/RecipeSection";
+import Reviews from "../components/recipe-layout/Reviews";
+import Contact from "../components/recipe-layout/Contact";
+
 export default function Recipe() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Hero />
+      <RecipeSection />
+      <Reviews />
+      <Contact />
+    </>
+  );
 }
