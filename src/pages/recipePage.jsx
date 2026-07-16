2e4333281f74608dcdfc6c15d8e78e1ac89fafe8
BELLO RILIWAN <belloriliwan001@gmail.com>
2026-07-16 09:27:58 +0100

    Revert "Backend search api"

--- src/pages/recipePage.jsx
+++ src/pages/recipePage.jsx
@@ -1,24 +1,15 @@
-import { useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
 import Hero from "../components/recipePage/Hero";
 import RecipeSection from "../components/recipePage/RecipeSection";
 import Testimonial from "../components/common/testimonial";
 import ContactForm from '../components/common/contactForm';
 
 export default function Recipe() {
-  const [searchParams] = useSearchParams();
-  const id = searchParams.get("id");
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [id]);
-
   return (
-    <div style={{ paddingTop: id ? "100px" : "0" }}>
-      {!id && <Hero />}
+    <>
+      <Hero />
       <RecipeSection />
       <Testimonial />
       <ContactForm />
-    </div>
+    </>
   );
-}
+} 
