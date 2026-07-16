459bab201be7a1a95c831ca35f57151c26ae75ca
Praise18100 <akenroyepraise@gmail.com>
2026-07-16 09:36:46 +0100

    Add esbuild dependency and update Vite configuration for CSS minification

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    build: {
+    // Force Vite to use esbuild instead of lightningcss for stylesheets
+    cssMinify: 'esbuild', 
+  },
 })
